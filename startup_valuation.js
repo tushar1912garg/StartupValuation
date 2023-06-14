770ab834bd8ef52e4515027d27b9f5037b123061
tushar1912garg <136434002+tushar1912garg@users.noreply.github.com>
2023-06-14 13:59:03 +0530

    Update startup_valuation.js

--- startup_valuation.js
+++ startup_valuation.js
@@ -9,10 +9,22 @@ function calculateValuation() {
 
   var valuation = calculate_startup_valuation(revenue, growthRate, industryMultiplier, opportunityCost, tam, sam, som);
 
-  document.getElementById("result").innerText = "The startup valuation is $" + valuation;
+  if (valuation !== null) {
+    document.getElementById("result").innerText = "The startup valuation is $" + valuation;
+  } else {
+    document.getElementById("result").innerText = "Invalid stage entered. Please choose 'idea', 'prototype', or 'revenue'.";
+  }
 }
 
 function calculate_startup_valuation(revenue, growth_rate, industry_multiplier, opportunity_cost, tam, sam, som) {
-  // Add your startup valuation calculation logic here
-  // Return the calculated valuation
+  if (stage === 'idea') {
+    return tam * 0.05;
+  } else if (stage === 'prototype') {
+    return tam * 0.1;
+  } else if (stage === 'revenue') {
+    var valuation = revenue * (1 + growth_rate) * industry_multiplier - opportunity_cost;
+    return valuation * (sam / som);
+  } else {
+    return null;
+  }
 }
